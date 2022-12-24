2e31eae0334a9004119004ccfe42abd474260f4c
HwnagJeongHun <ghkdwjdgns15@gmail.com>
2022-12-24 15:55:27 +0900

    Added-Utility(add-Menu-by-file)

--- Menu/Beverage.java
+++ Menu/Beverage.java
@@ -1,31 +1,52 @@
 package Menu;
 
+import java.util.List;
+import java.util.Map;
+
 public class Beverage {
-    enum SIZE {TALL, GRANDE, VENTI};
-    private String name;
+    private final String kind;
+    private final String name;
+    private final int basePrice;
+    private final List<String> sizeList;
+
     private String size;
     private int price;
 
-    private String kinds;
+    //private Map<String,Integer> priceBySize;
 
-    public Beverage(String name, String size, String kinds) {
+    //소비자용
+    /*public Beverage(String name, String size, String kind, int basePrice) {
         this.name = name;
-        this.size = size;
-        this.kinds = kinds;
+        //this.size = size;
+        this.kind = kind;
+        this.basePrice = basePrice;
         getPriceBySize(size);
        // basePriceBykinds();
+    }*/
+
+    //관리자용
+    Beverage(String name, String kind, int basePrice, List<String> sizeList){
+        this.name = name;
+        this.kind = kind;
+        this.basePrice = basePrice;
+        this.sizeList = sizeList;
+        //this.priceBySize = priceBySize;
     }
 
     public String getName(){
         return name;
     }
 
+    public String getKind(){return kind;}
+
+    public List<String> getSizeList(){return sizeList;}
+    public int getBasePrice(){return basePrice;}
     public void getPriceBySize(String size){
 
         switch(size){
-            case "TALL"->price = 4500;
-            case "GRANDE" -> price = 5000;
-            case "VENTI" -> price = 5500;
+            case "TALL"-> price = basePrice;
+            case "GRANDE" -> price = basePrice+500;
+            case "VENTI" -> price = basePrice+1000;
         }
     }
 
@@ -37,8 +58,14 @@ public class Beverage {
         this.size = size;
         getPriceBySize(size);
     }
-    @Override
+    /*@Override
     public String toString(){
         return "name:"+name + ", size:"+size;
+    }*/
+
+    //관리자테스트
+    @Override
+    public String toString(){
+        return "kind:"+kind+", name:"+name+", basePrice:"+basePrice+", sizeList" + getSizeList();
     }
 }
